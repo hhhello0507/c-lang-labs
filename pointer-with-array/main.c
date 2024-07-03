46c4e6e8759ca116caeab6871e35f6fb7cc6a27f
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-03 17:41:10 +0900

    Pointer With Array

--- pointer-with-array/main.c
+++ pointer-with-array/main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
 int main(void) {
+
     int arr[7] = {1, 2, 3, 4, 5, 6, 7};
     int *parr = &arr[0];
 
+    // 1
     for (int i = 0; i < 10; ++i) {
         printf("%d - %d - %d\n", parr + i, &arr[i], parr + i == &arr[i]);
     }
 
+    // 2
+    for (int i = 0; i < 10; ++i) {
+        printf("%d - %d - %d\n", parr + i, &i[arr], parr + i == &arr[i]);
+    }
+
+    int arr1[3] = {1, 2, 3};
+    printf("%d\n", 2[arr1]);
+
+    int arr2[3][3] = {
+        {
+            1, 2, 3
+        },
+        {
+            4, 5, 6
+        },
+        {
+            7, 8, 9
+        }
+    };
+
+    printf("%d", 2[1[arr2]]);
+
     return 0;
+
 }
